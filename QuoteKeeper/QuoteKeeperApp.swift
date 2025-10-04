80c62dd05a46fa61b011d2835958e1aef9bb548c
Tens1des <romankotov3213@gmail.com>
2025-10-04 03:14:45 +0300

    Initial Commit

---
+++ QuoteKeeper/QuoteKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuoteKeeperApp.swift
+//  QuoteKeeper
+//
+//  Created by Рома Котов on 04.10.2025.
+//
+
+import SwiftUI
+
+@main
+struct QuoteKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
